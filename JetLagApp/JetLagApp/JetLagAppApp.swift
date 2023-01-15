b4671dc1e9984a0f0cdb042eb995be929607400f
JIwonKwon <rnjswldnjs1012@gmail.com>
2023-01-15 10:46:15 +0900

    creative app

---
+++ JetLagApp/JetLagApp/JetLagAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JetLagAppApp.swift
+//  JetLagApp
+//
+//  Created by kwon ji won on 2023/01/15.
+//
+
+import SwiftUI
+
+@main
+struct JetLagAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
