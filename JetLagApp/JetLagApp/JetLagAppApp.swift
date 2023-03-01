87b89f384178f9079e1fbecfc96ff7afa0a88f2d
Shin <struct.developer@gmail.com>
2023-03-01 14:32:05 +0900

    feat: tabbar UI

--- JetLagApp/JetLagApp/JetLagAppApp.swift
+++ JetLagApp/JetLagApp/JetLagAppApp.swift
@@ -9,9 +9,32 @@ import SwiftUI
 
 @main
 struct JetLagAppApp: App {
+    
+    init() {
+        UITabBar.appearance().backgroundColor = UIColor(Color(hex: 0x2C2551))
+        UITabBar.appearance().unselectedItemTintColor = UIColor.white
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TabView {
+                ContentView()
+                    .tabItem {
+                        Label("홈", image: "tabbarHome")       
+                    }
+                ContentView()
+                    .tabItem {
+                        Label("일정", image: "tabbarCalendar")
+                    }
+                ContentView()
+                    .tabItem {
+                        Label("수면분석", image: "tabbarSleepAnalysis")
+                    }
+                ContentView()
+                    .tabItem {
+                        Label("설정", image: "tabbarSetting")
+                    }
+            }
         }
     }
 }
